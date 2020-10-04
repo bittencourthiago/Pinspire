fd88a4476f616a81eb381b911352cc45ef876a3f
Thiago Bittencourt Coelho <thiago.bittc@gmail.com>
2020-10-04 15:18:34 -0300

    Atualizando css
    Atualizando css da caixa de login e adicionando alguns comentários no código.

--- src/login.js
+++ src/login.js
@@ -1,14 +1,15 @@
-document.getElementById('loginPage').style.transitionDuration="0.45s";
+document.getElementById('loginPage').style.transitionDuration="0.45s"; //definindo o tempo de transição da página de login
+
 function openLogin() {
     let divLogin = document.getElementById('loginPage');
     divLogin.classList.add('loginNew');
     
     setTimeout(function(){
-        divLogin.style.display="block"
+        divLogin.style.display="block" // tirando o display none
     
         setTimeout(function(){
     
-            divLogin.style.opacity = "1"
+            divLogin.style.opacity = "1" //mostrando a página de login
     
         }, 400)
     
@@ -18,10 +19,11 @@ function closeLogin() {
     let divLogin = document.getElementById('loginPage');
 
     setTimeout(function(){
-        divLogin.style.opacity = "0"
+
+        divLogin.style.opacity = "0" //fazendo login ficar invisível
         setTimeout(function(){
     
-            divLogin.style.display="none"
+            divLogin.style.display="none" //deixando login com display none
     
         }, 400)
     }, 100);
--- src/pageReact.js
+++ src/pageReact.js
@@ -1,12 +1,14 @@
 var tempoGif = 2200; //2200
-document.querySelector('body').style.overflow="hidden";
+
+document.querySelector('body').style.overflow="hidden";//deixando o body sem navegação
 let loading = document.querySelector('#loading')
+
 function allCharge() {
         setTimeout(function(){
             loading.style.opacity = "0"
             setTimeout(function(){
-                loading.style.display = "none"
-                document.querySelector('body').style.overflow="auto";
+                loading.style.display = "none"// Desaparecendo a tela de carregamento
+                document.querySelector('body').style.overflow="auto"; //deixando o body navegável
             }, 500);
         }, tempoGif);
 }
